a90620042beec941b3803764868289ff2967d5a6
zeocax <zeocax@zeocax.com>
2025-04-13 12:44:58 +0000

    Backward Compatibility
    Signed-off-by: zeocax <zeocax@zeocax.com>

--- verl/workers/reward_manager/auto.py
+++ verl/workers/reward_manager/auto.py
@@ -5,6 +5,10 @@ REWARD_MANAGER_MAPPING = {
     "dapo": ("dapo", "DAPORewardManager"),
 }
 
+import logging
+import os
+logger = logging.getLogger(__name__)
+
 class AutoRewardManager:
 
     def __init__(self):
@@ -17,7 +21,18 @@ class AutoRewardManager:
         Args:
             reward_manager_name_or_custom_cls: the name of the reward manager or the custom class to the reward manager
         """
-        if reward_manager_name_or_custom_cls.custom_cls.get("path", None) is not None \
+        if isinstance(reward_manager_name_or_custom_cls, str):
+            if reward_manager_name_or_custom_cls in REWARD_MANAGER_MAPPING:
+                import importlib
+                # maybe we can lazy load the module here like transformers
+                module_name, class_name = REWARD_MANAGER_MAPPING[reward_manager_name_or_custom_cls]
+                module = importlib.import_module(f".{module_name}", package="verl.workers.reward_manager")
+                return getattr(module, class_name)
+            else:
+                raise ValueError(f"Invalid reward manager name: {reward_manager_name_or_custom_cls}, "
+                                 f"we have implemented the following reward managers: {REWARD_MANAGER_MAPPING.keys()}")
+            
+        elif reward_manager_name_or_custom_cls.custom_cls.get("path", None) is not None \
                 or reward_manager_name_or_custom_cls.custom_cls.get("name", None) is not None:
             if reward_manager_name_or_custom_cls.custom_cls.get("path", None) is None \
                 or reward_manager_name_or_custom_cls.custom_cls.get("name", None) is None:
@@ -27,18 +42,10 @@ class AutoRewardManager:
                 return load_extern_type(reward_manager_name_or_custom_cls.custom_cls.path, reward_manager_name_or_custom_cls.custom_cls.name)
             except Exception as e:
                 raise ValueError(f"Failed to load custom reward manager: {e}")
-            
-        elif reward_manager_name_or_custom_cls.get("name", None) is not None:
-            if reward_manager_name_or_custom_cls.name in REWARD_MANAGER_MAPPING:
-                import importlib
-                # maybe we can lazy load the module here like transformers
-                module_name, class_name = REWARD_MANAGER_MAPPING[reward_manager_name_or_custom_cls.name]
-                module = importlib.import_module(f".{module_name}", package="verl.workers.reward_manager")
-                return getattr(module, class_name)
-            else:
-                raise ValueError(f"Invalid reward manager name: {reward_manager_name_or_custom_cls}, "
-                                 f"we have implemented the following reward managers: {REWARD_MANAGER_MAPPING.keys()}")
-
         else:
-            raise ValueError(f"Please provide a valid reward manager name: {REWARD_MANAGER_MAPPING.keys()}"
-                             f"or a custom class (custom_cls) to the reward manager")
+            logger.log(msg=f"No reward manager is provided, using the default reward manager: 'naive'", level=logging.WARNING)
+            import importlib
+            # maybe we can lazy load the module here like transformers
+            module_name, class_name = REWARD_MANAGER_MAPPING["naive"]
+            module = importlib.import_module(f".{module_name}", package="verl.workers.reward_manager")
+            return getattr(module, class_name)
